80e75094372fcea38c8f51d325e79e4c0f8a6667
AjayShinde1999 <ajayshindevs07@gmail.com>
2023-08-06 15:57:34 +0530

    Updated controller

--- src/main/java/com/SampleSpringBoot/controller/SampleController.java
+++ src/main/java/com/SampleSpringBoot/controller/SampleController.java
@@ -1,5 +1,6 @@
 package com.SampleSpringBoot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,10 @@ public class SampleController {
     @RequestMapping("/hello")
     public String test(){
         return "hello";
+    }
+
+    @GetMapping("/view")
+    public String test2(){
+        return "Hello 3KT";
     }
 }
